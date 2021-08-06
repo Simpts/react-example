39c41fe18c7abb6c7ad06add267b925b22b43a6a
Simon S <contrib@simonstark.dev>
2021-08-06 19:27:50 +0200

    Prettify project

--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 4430;
 
 app.get("/", (_, res) => {
-  res.send("Hello World!")
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`)
+  console.log(`Listening at http://localhost:${port}`);
 });
