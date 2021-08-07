450446bd2d4cac9bd051d7ae7893efba1f4d02f0
Simon S <contrib@simonstark.dev>
2021-08-07 17:54:00 +0200

    Server: serve static React build files

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 import express from "express";
+import { URL } from "url";
+
+const staticDir = new URL("../client/build", import.meta.url).pathname;
+const fallbackPath = new URL("../client/build/index.html", import.meta.url)
+  .pathname;
+
 const app = express();
 const port = 4430;
 
-app.get("/", (_, res) => {
-  res.send("Hello World!");
+app.use(express["static"](staticDir));
+
+app.get("*", (req, res) => {
+  res.sendFile(fallbackPath);
 });
 
 app.listen(port, () => {
