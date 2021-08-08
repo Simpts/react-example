0d6e31b831f43570e199e839e7356ed9f62551da
Simon S <contrib@simonstark.dev>
2021-08-08 13:25:05 +0200

    Add expenseTracker exports

--- server/services/expenseTracker.js
+++ server/services/expenseTracker.js
@@ -1,1 +1,30 @@
-// @todo
+import mysql from "mysql2";
+
+const DB = mysql.createPool({
+  host: process.env.DB_HOST ?? "localhost",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+export function getExpenses() {
+  return new Promise((resolve, reject) => {
+    DB.execute("SELECT amount, description FROM expenses", (error, rows) => {
+      if (error) return reject(error);
+      resolve(rows);
+    });
+  });
+}
+
+export function addExpense(amount, description) {
+  return new Promise((resolve, reject) => {
+    DB.execute(
+      "INSERT INTO expenses (amount, description) VALUES (?, ?)",
+      [amount, description],
+      error => {
+        if (error) return reject(error);
+        resolve();
+      },
+    );
+  });
+}
