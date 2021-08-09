4f671c8f0c885e1d2c1a627a666b8f5b857b5ac1
Simon S <contrib@simonstark.dev>
2021-08-09 19:17:14 +0200

    Create ExpenseSummary & ExpenseTable components

--- client/src/components/ExpenseSummary.js
+++ client/src/components/ExpenseSummary.js
@@ -1,1 +1,14 @@
-// @todo
+import { formatCurrency } from "../lib/currency";
+
+export default function Summary(props) {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold mb-4 text-blue-800">
+        Totala utgifter:
+      </h1>
+      <span className="text-4xl font-extralight">
+        {formatCurrency(props.total ?? 0)}
+      </span>
+    </div>
+  );
+}
--- client/src/components/ExpenseTable.js
+++ client/src/components/ExpenseTable.js
@@ -1,1 +1,46 @@
-// @todo
+import { formatCurrency } from "../lib/currency";
+const COLUMNS = ["Beskrivning", "Utgift"];
+
+export default function ExpenseTable(props) {
+  return (
+    <table
+      className="border-collapse w-full rounded-2xl"
+      aria-labelledby="table-title"
+    >
+      <thead className="bg-blue-700 text-white">
+        <tr>
+          {COLUMNS.map((col, index) => (
+            <th
+              className={`text-left py-2 ${index === 0 ? "pl-4" : ""}`}
+              key={col}
+              scope="col"
+            >
+              {col}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody className="py-4 bg-gray-50">
+        {props.rows.map((expense, index) => (
+          <tr
+            className={
+              index !== props.rows.length - 1 ? "border-b border-gray-300" : ""
+            }
+            key={index}
+          >
+            <th
+              className={`w-full text-left font-light text-black pl-4 ${
+                !expense.description ? "text-opacity-60 italic" : ""
+              } `}
+              role="cell"
+              scope="row"
+            >
+              {expense.description || "Allmänn utgift"}
+            </th>
+            <td className="pr-4 py-2">{formatCurrency(expense.amount)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
