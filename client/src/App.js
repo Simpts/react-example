0c776c1eb1603625df6855bf0c5e323d05e74b2a
Simon S <contrib@simonstark.dev>
2021-08-09 18:50:06 +0200

    Sketch out UI, add mock data

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,32 @@
+import Summary from "./components/ExpenseSummary";
+import ExpenseTable from "./components/ExpenseTable";
+import ExpenseForm from "./components/ExpenseForm";
+import { safeSum } from "./lib/currency";
+
 function App() {
-  return <div class="font-mono">Hello World</div>;
+  const fakeData = [
+    {
+      amount: 299.99,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    { amount: 25, description: "Lo" },
+    { amount: 42.5, description: "" },
+    { amount: 239657.5 },
+  ];
+
+  return (
+    <div className="min-h-screen w-screen max-w-xl mx-auto flex flex-col items-start justify-center px-4">
+      <Summary total={safeSum(fakeData.map(({ amount }) => amount))} />
+      <div className="w-full my-10">
+        <h3 id="table-title" className="font-semibold text-xl">
+          Senaste utgifterna
+        </h3>
+        <hr className="my-4" />
+        <ExpenseTable rows={fakeData} />
+      </div>
+      <ExpenseForm />
+    </div>
+  );
 }
 
 export default App;
